62c1f5d27bf636317ead4eaa7a66928491d4ccf9
Garbo Alan <39155304+NalaGarbo@users.noreply.github.com>
2021-04-01 16:39:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,10 @@ chat = [
 res = ta.tone_chat(chat).get_result()
 
 print (res)
+
+# result down below:
+
+# {'utterances_tone': [{'utterance_id': 0, 'utterance_text': 'What a beautiful day to be the only club to have won the champions leagues in France.', 'tones': [{'score': 0.977044, 'tone_id': 'excited', 'tone_name': 'Excited'}, {'score': 
+# 0.775363, 'tone_id': 'satisfied', 'tone_name': 'Satisfied'}]}, {'utterance_id': 1, 'utterance_text': "This sucks, we have invested more than 1 bilion euro
+# into theses clowns and still haven't won any european championship", 'tones': [{'score': 0.777998, 'tone_id': 'frustrated', 'tone_name': 'Frustrated'}, 
+# {'score': 0.87061, 'tone_id': 'impolite', 'tone_name': 'Impolite'}, {'score': 0.62453, 'tone_id': 'sad', 'tone_name': 'Sad'}]}]}
